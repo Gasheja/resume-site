a3333f30beb2a930e7853030d75bc8a020b2d8fc
Gasheja <m.gasheja@alustudent.com>
2024-02-20 23:42:44 -0800

    resume site

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# resume-site
+This is my resume containing the projects that I have been working on, experience, and contact information for those who want to connect with me via email or linkedin
---
+++ script.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".run-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
